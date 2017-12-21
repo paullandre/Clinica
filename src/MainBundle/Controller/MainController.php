93556f8aa2aca1e08fec72015d3b18fc199efafa
paullandre <erdan08@yahoo.com>
2017-12-21 15:17:14 +0800

    Add files

---
+++ src/MainBundle/Controller/MainController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace MainBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Component\HttpFoundation\Response;
+
+class MainController extends Controller
+{
+    /**
+     * @Route("/main/{name}")
+     * @Template()
+     */
+    public function indexAction($name)
+    {
+        return array('name' => $name);
+    }
+	
+	/**
+     * @Route("/lucky/number")
+     */
+    public function numberAction()
+    {
+        $number = mt_rand(0, 100);
+
+        return new Response(
+            '<html><body>Lucky number: '.$number.'</body></html>'
+        );
+    }
+}
